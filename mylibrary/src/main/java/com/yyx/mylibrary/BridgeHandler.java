52ad0c97ccd73170daffdb41537b0bacf423ba80
vimi8 <466824141>
2017-05-23 11:27:16 +0800

    yyx

---
+++ mylibrary/src/main/java/com/yyx/mylibrary/BridgeHandler.java
@@ -0,0 +1,7 @@
+package com.yyx.mylibrary;
+
+public interface BridgeHandler {
+	
+	void handler(String data, CallBackFunction function);
+
+}
---
+++ mylibrary/src/main/java/com/yyx/mylibrary/CallBackFunction.java
@@ -0,0 +1,7 @@
+package com.yyx.mylibrary;
+
+public interface CallBackFunction {
+	
+	public void onCallBack(String data);
+
+}
